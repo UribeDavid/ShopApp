87af11fe7b0b246e5b7d44c0d03d88a219e9237e
David Uribe <ctcdesarrollo3@hotmail.com>
2021-07-11 13:24:30 -0500

    Deleted unused functions

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -22,24 +22,12 @@ export class CartService {
     return this.firestore.collection('carts').doc(order.id).update({status: order.status});
   }
 
-  deleteOrder(id: string) {
-    return this.firestore.collection('carts').doc(id).delete();
-  }
-
   getAllProductsCart() {
     return this.firestore.collection('product_cart').snapshotChanges();
   }
 
-  getProductsByCart(id: string) {
-    return this.firestore.collection('product_cart', ref => ref.where('cart_id', '==', id)).snapshotChanges();
-  }
-
   addProductCart(product: Product_Cart) {
     return this.firestore.collection('product_cart').add(product);
-  }
-
-  deleteCartProduct(id: string) {
-    return this.firestore.collection('product_cart', ref => ref.where('cart_id', '==', id)).doc().delete();
   }
 
 }
